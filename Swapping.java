63f2a4b2408d27e9413838042e0ba9b5afd77b56
yeswanth-8125 <yeswanthkumarelluru@gmail.com>
2025-01-29 00:56:13 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,13 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int a=13;
+        int b=12;
+        int c=0;
+        c=a;
+        a=b;
+        b=c;
+       
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
